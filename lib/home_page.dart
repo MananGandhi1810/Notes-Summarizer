00eafe512daa0dfad1be4563b85ab3ff7d437917
Manan Gandhi <ardumanan@gmail.com>
2024-07-29 23:50:02 +0530

    Sliver App Bar and changed colors

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -32,12 +32,9 @@ class _HomePageState extends State<HomePage> {
   ];
   final colors = [
     Colors.red[400],
-    Colors.green[400],
     Colors.blue[400],
-    Colors.purple[400],
-    Colors.orange[400],
-    Colors.pink[400],
-    Colors.teal[400],
+    Colors.green[400],
+    Colors.yellow[400],
   ];
 
   void _loadNotes() async {
@@ -72,7 +69,9 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         )
-        .then((value) => _loadNotes());
+        .then(
+          (value) => _loadNotes(),
+        );
   }
 
   @override
@@ -84,75 +83,89 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: const Text(
-          'Notes',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-        ),
-      ),
-      body: _notes.isEmpty
-          ? const Center(
-              child: Text(
-                'No notes found',
-                style: TextStyle(fontSize: 24),
-              ),
-            )
-          : GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-              ),
-              physics: const BouncingScrollPhysics(),
-              itemCount: _notes.length,
-              itemBuilder: (context, index) {
-                final lastEdited = _notes[index].lastEdited;
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    color: colors[index % colors.length],
-                    child: InkWell(
-                      onTap: () => {
-                        Navigator.of(context)
-                            .push(
-                              MaterialPageRoute(
-                                builder: (context) => EditPage(
-                                  id: _notes[index].id,
-                                ),
-                              ),
-                            )
-                            .then((value) => _loadNotes())
-                      },
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              _notes[index].title,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          Expanded(child: Container()),
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              "${months[lastEdited.month - 1]} ${lastEdited.day}, ${lastEdited.year}",
-                              style: const TextStyle(
-                                fontSize: 16,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+      backgroundColor: Colors.black,
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+              stretch: true,
+              elevation: 0,
+              expandedHeight: MediaQuery.of(context).size.height * 0.2,
+              flexibleSpace: const FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text(
+                  'Notes',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              )),
+          _notes.isEmpty
+              ? const SliverFillRemaining(
+                  child: Center(
+                    child: Text(
+                      'No notes found',
+                      style: TextStyle(fontSize: 24),
                     ),
                   ),
-                );
-              },
-            ),
+                )
+              : SliverGrid.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                  ),
+                  itemCount: _notes.length,
+                  itemBuilder: (context, index) {
+                    final lastEdited = _notes[index].lastEdited;
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: colors[index % colors.length],
+                        child: InkWell(
+                          onTap: () => {
+                            Navigator.of(context)
+                                .push(
+                                  MaterialPageRoute(
+                                    builder: (context) => EditPage(
+                                      id: _notes[index].id,
+                                    ),
+                                  ),
+                                )
+                                .then((value) => _loadNotes())
+                          },
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  _notes[index].title,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              Expanded(child: Container()),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  "${months[lastEdited.month - 1]} ${lastEdited.day}, ${lastEdited.year}",
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _newNote,
         backgroundColor: colors[_notes.length % colors.length],
